5aeec1bc5d1b12a51599ec5b71aadc188657d8e5
nadiah-m <nadiah.malik@Hotmail.com>
2025-02-04 18:55:19 +0800

    update cors

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //http://localhost:8080/api/v1/orders
-const host = "http://flowcart.local";
+const host = "http://flowcart.ndhmlk.site/";
 const ORDER_SERVICE_URL = host + "/api/v1/orders";
 const STOCK_SERVICE_URL = host + "/api/stock";
 
